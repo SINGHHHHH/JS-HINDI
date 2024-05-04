5921b6151542092c40b5cb255ed9d3590d6bbb22
KIRAT SINGH <99126277+SINGHHHHH@users.noreply.github.com>
2024-05-04 11:07:27 +0000

    variables

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,8 @@
+const accountId = 12345;
+let accountEmail = 'ankitamatharu@gmail.com';
+var accountPassword = 1469;
+accountCity = 'kashmir';
+let accountState;
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
+console.log(accountEmail);
