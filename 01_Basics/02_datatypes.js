85a23862f8ba2fd86099551e694b42681abb2c5d
KIRAT SINGH <99126277+SINGHHHHH@users.noreply.github.com>
2024-05-04 11:08:00 +0000

    datatypes

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,17 @@
+"use strict"; // treat all JS Code as newer version
+// alert("hello"); // we ae using NodeJS, not browser
+// data types in js
+let name = "kirat"; // string 
+let age = 18; // number
+let loggedIn = false; // boolean
+// number
+// bigint
+// string
+// null => standalone value
+// boolean => true/false
+// undefined 
+// symbol => for uniqueness 
+// object
+
+console.log("type of null: " + typeof null);
+console.table([name, age,loggedIn]);
